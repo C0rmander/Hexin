dc18063b5f898ba742a3c7e2d36c9bd49996755b
Cormac Conway <71558726+C0rmander@users.noreply.github.com>
2021-12-11 03:28:51 +0000

    first commit

---
+++ hexes/lavaHex.go
@@ -0,0 +1,40 @@
+components {
+  id: "HexScript"
+  component: "/hexes/HexScript.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "colonised"
+    value: "false"
+    type: PROPERTY_TYPE_BOOLEAN
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/hexes/hexAtlas.atlas\"\n"
+  "default_animation: \"lava_hex\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
